982e621de94dea4abfa83117a2727d4148cd6b18
themmixproject <steven.devalk@gmail.com>
2023-08-09 17:59:53 +0200

    Updated some things to increase things to test

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,5 @@
 import "../scss/main.scss";
+
+document.getElementById("click-button").addEventListener("click", function(){
+    console.log("I am the button on the page and I have been clicked!");
+})
